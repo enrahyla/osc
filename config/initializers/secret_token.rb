b17566a7574e42f7f77071f4dfbd3509f5cc1782
enrahyla <A.D.Hyland@2009.ljmu.ac.uk>
2013-02-01 13:23:25 +0000

    Assignment 2
    Assignment 2 index page working

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Osc::Application.config.secret_token = 'e00deadf09c6a6028774e3e091a11495cff61b0b587b2106abb4b3a56b3fdb2a8503de48333ac6b4e6b35ff422d4bf7d273bb7baf29d83701caeb39497f298ec'
